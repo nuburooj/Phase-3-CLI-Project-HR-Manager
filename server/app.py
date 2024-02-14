b6f3db6c6866d65d2afbdc15355854b93d52420a
nuburooj <nuburoojkhattak@gmail.com>
2024-02-14 14:50:27 -0500

    implemented functionality to look up franchise info by id and made a submenu for adding and seeing a franchise's employees (functionality still pending)

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 from config import app, migrate
 from rich import print
 from models import db
-from utils import get_all_franchises
+from utils import get_all_franchises, search_franchise_by_id
 
 def welcome_screen():
   print("[yellow]Hello! Welcome to [/yellow][bold cyan]HR Manager[/bold cyan]")
@@ -18,7 +18,30 @@ def get_main_choice():
 def display_all_franchises():
   franchises = get_all_franchises()
   for franchise in franchises:
-    print(f"{franchise.id} | {franchise.branch_name} | {franchise.franchise_no} | {franchise.date_opened}")
+    print(f"{franchise.id} | {franchise.branch_name} |")
+  print("[bold green]What do you wanna do?[/]")
+  print("1. See more about a franchise")
+  print("2. Return to Main Menu")
+  choice = input()
+  if choice == "1":
+    choose_franchise_by_id()
+  else:
+    return 
+  
+def choose_franchise_by_id():
+  search_id = input("Enter the id of the franchise you'd like to know more about: ")
+  franchise = search_franchise_by_id(search_id)
+  print(f"{franchise.id} | {franchise.branch_name} | {franchise.franchise_no} | {franchise.date_opened} |")
+  display_franchise_submenu(franchise)
+
+def display_franchise_submenu(franchise):
+  print("[bold green]What would you like to do?[/]")
+  print("1. See a Franchise's Employees")
+  print("2. Add an Employee to a Franchise")
+  print("3. Exit")
+  choice = input()
+
+
 
 
 if __name__ == "__main__":
--- server/utils.py
+++ server/utils.py
@@ -4,3 +4,6 @@ from models import Franchise, Employee
 
 def get_all_franchises():
     return db.session.query(Franchise).all()
+
+def search_franchise_by_id(id):
+    return db.session.get(Franchise, id)
