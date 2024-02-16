4ba0d69f5da20a2cb1b6a797d121a6acdb961d18
nuburooj <nuburoojkhattak@gmail.com>
2024-02-16 13:05:50 -0500

    made helper functions in util.py to search franchises and employees by id, also made helper functions to change employees jobs and adding employee to franchise and deleting employees from franchise, implemented them in app.py in the handle_employee_choice function, aswell as made a seperate sub-menu for employee operations. Made some visual changes to the text prompts in terminal with rich.

--- server/app.py
+++ server/app.py
@@ -1,48 +1,94 @@
 from config import app, migrate
 from rich import print
 from models import db
-from utils import get_all_franchises, search_franchise_by_id
+from utils import get_all_franchises, search_franchise_by_id, get_all_employees, add_employee_to_franchise, fire_employee_from_franchise, search_employee_by_id, change_employee_job
 
 def welcome_screen():
   print("[yellow]Hello! Welcome to [/yellow][bold cyan]HR Manager[/bold cyan]")
 
+
 def display_main_menu():
   print("[bold green]Main Menu[/]")
-  print("1. Show all Franchises")
-  print("2. Add a Franchise")
-  print("3. Exit")
+  print("1. [bold]Show all Franchises[/]")
+  print("2. [bold]Add a Franchise[/]")
+  print("3. [bold red]Exit[/]")
+
 
 def get_main_choice():
-  return input("What's it gonna be?")
+  return input("What's it gonna be? ")
+
 
 def display_all_franchises():
   franchises = get_all_franchises()
   for franchise in franchises:
-    print(f"{franchise.id} | {franchise.branch_name} |")
+    print(f"{franchise.id} | [yellow]Location:[/] [bold cyan]{franchise.branch_name}[/] |")
   print("[bold green]What do you wanna do?[/]")
-  print("1. See more about a franchise")
-  print("2. Return to Main Menu")
+  print("1. [bold]See more about a franchise[/]")
+  print("2. [bold]Return to Main Menu[/]")
   choice = input()
   if choice == "1":
     choose_franchise_by_id()
   else:
     return 
   
+
 def choose_franchise_by_id():
   search_id = input("Enter the id of the franchise you'd like to know more about: ")
   franchise = search_franchise_by_id(search_id)
-  print(f"{franchise.id} | {franchise.branch_name} | {franchise.franchise_no} | {franchise.date_opened} |")
+  print(f"{franchise.id} | [yellow]Location:[/] [bold cyan]{franchise.branch_name}[/] | [yellow]Franchise no.[/] {franchise.franchise_no} | [yellow]Inaugurated:[/] {franchise.date_opened} |")
   display_franchise_submenu(franchise)
 
+
 def display_franchise_submenu(franchise):
-  print("[bold green]What would you like to do?[/]")
-  print("1. See a Franchise's Employees")
-  print("2. Add an Employee to a Franchise")
-  print("3. Exit")
+  print("[bold yellow]What would you like to do?[/]")
+  print("1. [bold]See a Franchise's Employees[/]")
+  print("2. [bold]Add an Employee to a Franchise[/]")
+  print("3. [bold green]Main menu[/]")
   choice = input()
+  handle_franchise_choice(choice, franchise)
 
 
+def handle_franchise_choice(choice, franchise):
+  if choice == "1":
+    show_franchise_employees(franchise)
+  elif choice == "2":
+    add_employee_to_franchise(franchise)
+  else:
+    return
 
+
+def show_franchise_employees(franchise):
+  employees = get_all_employees()
+  for employee in employees:
+    if franchise.id == employee.franchise_id:
+      print(f"{employee.id} | [yellow]Name:[/] [bold cyan]{employee.name}[/] | [yellow]Job:[/] [bold cyan]{employee.job}[/] | [yellow]Hired on:[/] {employee.hiring_date} | [yellow]Franchise ID:[/] {employee.franchise_id} |" )
+  choose_employee_by_id()
+
+
+def choose_employee_by_id():
+    search_id = input("Enter the id of the Employee for options: ")
+    employee = search_employee_by_id(search_id)
+    display_employee_submenu(employee)
+
+
+def display_employee_submenu(employee):
+    print("1. [bold red]Fire[/] employee")
+    print("2. [bold yellow]Update[/] employee job")
+    print("3. [bold green]Main Menue[/]")
+    choice = input()
+    handle_employee_choice(choice, employee)
+
+    
+def handle_employee_choice(choice, employee):
+  if choice == "1":
+    fire_employee_from_franchise(employee)
+    print("[italic yellow]Employee has been let go.[/]")
+  elif choice == "2":
+    change_employee_job(employee)
+    print("[italic yellow]Employee just got promoted or demoted. (it's all about perspective)[/]")
+  else:
+    return
+  
 
 if __name__ == "__main__":
   with app.app_context():
--- server/utils.py
+++ server/utils.py
@@ -1,9 +1,55 @@
 from config import db
 from models import Franchise, Employee
-
+from pick import pick
+from datetime import datetime
+from rich import print
 
 def get_all_franchises():
     return db.session.query(Franchise).all()
 
+
 def search_franchise_by_id(id):
     return db.session.get(Franchise, id)
+
+
+def search_employee_by_id(id):
+    return db.session.get(Employee, id)
+
+
+def get_all_employees():
+    return db.session.query(Employee).all()
+
+
+def change_employee_job(employee):
+    req_type = "What job is this Employee gonna be working?"
+    new_job, index = pick(["Manager", "Frier", "Cashier"], req_type)
+    employee.job = new_job
+    db.session.commit()
+
+
+def add_employee_to_franchise(franchise):
+    employee_names = [employee.name for employee in db.session.query(Employee).all()]
+    prompt = "Which employee is gonna be working at this franchise?"
+    employee_name, index = pick(employee_names, prompt)
+    employee = db.session.query(Employee).filter(Employee.name == employee_name).first()
+    req_type = "What job is this Employee gonna be working?"
+    job, index = pick(["Manager", "Frier", "Cashier"], req_type)
+    date_string = input("Enter date and time to sign employment contract in the format YYYY-MM-DD HH:MM:SS: ")
+    date = datetime.strptime(date_string, "%Y-%m-%d %H:%M:%S")
+    employee = Employee(
+        name=employee_name,
+        franchise_id=franchise.id,
+        job=job,
+        hiring_date=date,
+       
+    )
+    print("[italic yellow]Employee has been hired![/]")
+
+    db.session.add(employee)
+    db.session.commit()
+
+
+def fire_employee_from_franchise(employee):
+    db.session.delete(employee)
+    db.session.commit()
+    
